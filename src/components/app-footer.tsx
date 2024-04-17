31e337ad6b01179b8fd401ad5a958d918b75810e
cherrydub <cherrydub@gmail.com>
2024-04-17 18:18:22 +0100

    seperated out some content within its own components, such as h1, branding, stats

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function AppFooter() {
-  return <div>app-footer</div>;
+  return (
+    <footer className="mt-auto border-t border-black/5 py-5">
+      <small className="opacity-50">
+        &copy; {new Date().getFullYear()} ChrisCoding.xyz. All rights reserved
+      </small>
+    </footer>
+  );
 }
