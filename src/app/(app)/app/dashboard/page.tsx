fa8a061bd433063a68a31c9012d25c7a6afa97f4
cherrydub <cherrydub@gmail.com>
2024-04-17 18:26:40 +0100

    the start of the structure of the dashboard, many components added

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,4 +1,8 @@
 import Branding from "@/components/branding";
+import ContentBlock from "@/components/content-block";
+import PetDetails from "@/components/pet-details";
+import PetList from "@/components/pet-list";
+import SearchForm from "@/components/search-form";
 import Stats from "@/components/stats";
 import React from "react";
 
@@ -8,6 +12,16 @@ export default function Dashboard() {
       <div className="flex items-center justify-between text-white py-8">
         <Branding />
         <Stats />
+      </div>
+      <div>
+        <SearchForm />
+        <ContentBlock>
+          <PetList />
+        </ContentBlock>
+
+        <ContentBlock>
+          <PetDetails />
+        </ContentBlock>
       </div>
     </main>
   );
