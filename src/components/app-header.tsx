7e409bac3ee34747ac0343f4b066115cf1fdb99f
cherrydub <cherrydub@gmail.com>
2024-04-17 17:46:14 +0100

    within the folder structure of the app router, I have added a layout.tsx file which is exactly like the main layout file, you can structure a layout within the different routes

---
+++ src/components/app-header.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Logo from "./logo";
+import Link from "next/link";
+
+const routes = [
+  {
+    label: "Dashboard",
+    path: "/app/dashboard",
+  },
+  {
+    label: "Account",
+    path: "/app/account",
+  },
+];
+
+export default function AppHeader() {
+  return (
+    <header className="flex justify-between items-center border-b border-white/10 py-2">
+      <Logo />
+      <nav>
+        <ul className="flex gap-2 text-xs">
+          {routes.map((route) => (
+            <li key={route.path}>
+              <Link
+                className="text-white/70 bg-black/10 rounded-sm px-2 py-1 hover:text-white focus:text-white transition "
+                href={route.path}
+              >
+                {route.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
