6c52842b1e65197da2cf932b109f135a958a6649
cherrydub <cherrydub@gmail.com>
2024-04-18 13:50:41 +0100

    more derived state such as petcount

--- src/contexts/pet-context-provider.tsx
+++ src/contexts/pet-context-provider.tsx
@@ -12,6 +12,7 @@ type TPetContext = {
   pets: Pet[];
   selectedPetId: string | null;
   selectedPet: Pet | undefined;
+  numberOfPets: number;
   handleChangeSelectedPetId: (id: string) => void;
 };
 
@@ -27,6 +28,7 @@ export default function PetContextProvider({
 
   //derived state
   const selectedPet = pets.find((pet) => pet.id === selectedPetId);
+  const numberOfPets = pets.length;
 
   //event handlers / actions
   function handleChangeSelectedPetId(id: string) {
@@ -35,7 +37,13 @@ export default function PetContextProvider({
 
   return (
     <PetContext.Provider
-      value={{ pets, selectedPetId, selectedPet, handleChangeSelectedPetId }}
+      value={{
+        pets,
+        selectedPetId,
+        selectedPet,
+        numberOfPets,
+        handleChangeSelectedPetId,
+      }}
     >
       {children}
     </PetContext.Provider>
