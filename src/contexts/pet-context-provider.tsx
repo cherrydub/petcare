0cab63ab073134b22e79c127d53c1d9e866f8dca
cherrydub <cherrydub@gmail.com>
2024-04-18 11:36:36 +0100

    finishing up pet context, have to add the types for children and the data thats fetched, along with the values that will be getting passed in the context

--- src/contexts/pet-context-provider.tsx
+++ src/contexts/pet-context-provider.tsx
@@ -1,17 +1,29 @@
 "use client";
 
+import { Pet } from "@/lib/types";
 import React, { createContext, useState } from "react";
 
-export const PetContext = createContext(null);
+type PetContextProviderProps = {
+  children: React.ReactNode;
+  data: Pet[];
+};
 
-export default function PetContextProvider({ data, children }) {
+type TPetContext = {
+  pets: Pet[];
+  selectedPetId: string | null;
+};
+
+export const PetContext = createContext<TPetContext | null>(null);
+
+export default function PetContextProvider({
+  data,
+  children,
+}: PetContextProviderProps) {
   const [pets, setPets] = useState(data);
   const [selectedPetId, setSelectedPetId] = useState(null);
 
   return (
-    <PetContext.Provider
-      value={{ pets, setPets, selectedPetId, setSelectedPetId }}
-    >
+    <PetContext.Provider value={{ pets, selectedPetId }}>
       {children}
     </PetContext.Provider>
   );
