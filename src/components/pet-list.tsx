5a8135911930b0078e395132ba6cd277bafbe89e
cherrydub <cherrydub@gmail.com>
2024-04-18 09:56:32 +0100

    adding Pet and PetListProps types

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,43 +1,31 @@
+import { Pet } from "@/lib/types";
 import Image from "next/image";
 import React from "react";
 
-export default function PetList() {
+type PetListProps = {
+  pets: Pet[];
+};
+
+export default function PetList({ pets }: PetListProps) {
   return (
     <ul className="bg-white border-b border-black/[0.08]">
-      <li>
-        <button
-          className="flex h-[70px] w-full cursor-pointer items-center px-5 text-base gap-3
+      {pets.map((pet) => (
+        <li key={pet.id}>
+          <button
+            className="flex h-[70px] w-full cursor-pointer items-center px-5 text-base gap-3
         hover:bg-[#eff1f2] focus:bg-[#eff1f2] transition"
-        >
-          <Image
-            src={
-              "https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
-            }
-            alt="Dog"
-            width={45}
-            height={45}
-            className="rounded-full object-cover"
-          />
-          <p className="font-semibold">Benjamin</p>
-        </button>
-      </li>
-      <li>
-        <button
-          className="flex h-[70px] w-full cursor-pointer items-center px-5 text-base gap-3
-        hover:bg-[#eff1f2] focus:bg-[#eff1f2] transition"
-        >
-          <Image
-            src={
-              "https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
-            }
-            alt="Dog"
-            width={45}
-            height={45}
-            className="rounded-full object-cover"
-          />
-          <p className="font-semibold">Stella</p>
-        </button>
-      </li>
+          >
+            <Image
+              src={pet.imageUrl}
+              alt="Dog"
+              width={45}
+              height={45}
+              className="rounded-full object-cover w-[45px] h-[45px]"
+            />
+            <p className="font-semibold">{pet.name}</p>
+          </button>
+        </li>
+      ))}
     </ul>
   );
 }
